9acdacc66973749f2087eda2c2978ea17971036d
gpryan50-star <gpryan50@gmail.com>
2026-04-15 11:59:15 -0500

    Add files via upload
    added code

---
+++ Badger Deals/js/app.js
@@ -0,0 +1,15 @@
+function calculateSavings() {
+  const price = document.getElementById('price').value;
+  const discount = document.getElementById('discount').value;
+
+  if (!price || !discount) {
+    document.getElementById('result').innerText = "Please fill out both fields.";
+    return;
+  }
+
+  const savings = price * (discount / 100);
+  const finalPrice = price - savings;
+
+  document.getElementById('result').innerText =
+    `You save $${savings.toFixed(2)}. Final price: $${finalPrice.toFixed(2)}`;
+}
